e94f44561df4b7e9d274f3674884e69169a61259
Sywurm <sybrandtvniekerk@gmail.com>
2023-10-29 18:42:41 +0200

    Added DontDestroyOnLoadScript

---
+++ Assets/Test Environment/SybrandtTest/Audio/DontDestroyOnLoad.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public string objectID;
+
+    private void Awake()
+    {
+        objectID = name + transform.position.ToString();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < Object.FindObjectsOfType<DontDestroyOnLoad>().Length; i++) 
+        {
+            if (Object.FindObjectsOfType<DontDestroyOnLoad>()[i] != this)
+            {
+                if (Object.FindObjectsOfType<DontDestroyOnLoad>()[i].objectID == objectID)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
